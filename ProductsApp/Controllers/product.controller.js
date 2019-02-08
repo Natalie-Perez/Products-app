1ed2d22773acc27434150ab046cee0c641997cc6
Natalie Perez <NPerez.dev@gmail.com>
2019-02-08 00:30:54 -0500

    added CRUD functionality.

--- ProductsApp/Controllers/product.controller.js
+++ ProductsApp/Controllers/product.controller.js
@@ -4,3 +4,44 @@ const Product = require('../models/product.model');
 exports.test = function (req, res) {
     res.send('Hello from the Test controller!');
 };
+
+// Create new product using the data coming from a POST request and save to database
+exports.product_create = function (req, res) {
+    let product = new Product(
+        {
+            name: req.body.name,
+            price: req.body.price
+        }
+    );
+
+    product.save(function (err) {
+        if (err) {
+            return next(err);
+        }
+        res.send('Product Created successfully')
+    })
+};
+
+// Read an existing product from the product ID being sent in the request.
+exports.product_details = function (req, res) {
+    Product.findById(req.params.id, function (err, product) {
+        if (err) return next(err);
+        res.send(product);
+    })
+};
+
+// Find an existing product using its ID that was sent in the request
+exports.product_update = function (req, res) {
+    Product.findByIdAndUpdate(req.params.id, { $set: req.body }, function (err, product) {
+        if (err) return next(err);
+        res.send('Product updated.');
+    })
+};
+
+// Deletes an existing product
+exports.product_delete = function (req, res) {
+    Product.findByIdAndRemove(req.params.id, function (err) {
+        if (err) return next(err);
+        res.send('Deleted successfully!');
+    })
+};
--- ProductsApp/app.js
+++ ProductsApp/app.js
@@ -8,8 +8,8 @@ const app = express();
 //Set up mongoose connection
 const mongoose = require('mongoose');
 let dev_db_url = 'mongodb://someuser:abcd1234@ds127015.mlab.com:27015/products-app';
-let mongodb = process.env.MONGODB_URI || dev_db_url;
-mongoose.connect(mongoDB);
+let mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB, { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
 let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error: '));
@@ -26,3 +26,7 @@ let port = 1234;
 app.listen(port, () => {
     console.log('Server is up and running on port number ' + port);
 });
+
+
+
+/// Update to code:  exports.product_details = function (req, res, next){
