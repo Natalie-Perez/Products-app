d8455d31b5fa62ef5bb3e9b73b54c3bcad6643d9
Natalie Perez <NPerez.dev@gmail.com>
2019-02-07 23:22:39 -0500

    connected app to mLab databse/mongoose connection

--- ProductsApp/app.js
+++ ProductsApp/app.js
@@ -1,12 +1,24 @@
-// require ExpressJS body parser packages
 const express = require('express');
 const bodyParser = require('body-parser');
 
-// initialize express app
 const product = require('./routes/product.route'); // Imports routes for the products
 const app = express();
 
+// connecting app to mLab database
+//Set up mongoose connection
+const mongoose = require('mongoose');
+let dev_db_url = 'mongodb://someuser:abcd1234@ds127015.mlab.com:27015/products-app';
+let mongodb = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error: '));
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/products', product);
+
 
 // Set dedicated port and tell Express app to listen to that port
 let port = 1234;
